01961a31b73a6d46184470a7de479378e839b04d
Sergey T <sergey.topolenko@starofservice.com>
2017-05-14 17:23:47 +0300

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,21 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-const middlewares = [];
-if (process.env.NODE_ENV === `development`) {
-  middlewares.push(logger);
-}
+const handleMiddlewares = withLogger => (...middleware) => {
+  let middlewares = [...middleware];
+
+  if (withLogger && process.env.NODE_ENV === `development`) {
+    middlewares.push(logger);
+  }
+  return middlewares;
+};
+
+const withLogger = handleMiddlewares(true);
+const withOutLogger = handleMiddlewares(false);
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, ...middlewares)
+  applyMiddleware(...withLogger(thunk))
 );
 
 export default store;
